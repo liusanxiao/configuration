8293e8ffbfa83d7afa5000c3377e93459b8e39ae
liusx13 <liusx13@lenovo.com>
2018-09-27 14:17:59 +0800

    commit

---
+++ swagger配置类/SwaggerConfig.java
@@ -0,0 +1,52 @@
+package lenovo.pcsd.products.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by qudx1 on 2017/7/7.
+ 		 <dependency>
+            <groupId>io.springfox</groupId>
+            <artifactId>springfox-swagger2</artifactId>
+            <version>2.6.1</version>
+        </dependency>
+        <dependency>
+            <groupId>io.springfox</groupId>
+            <artifactId>springfox-swagger-ui</artifactId>
+            <version>2.6.1</version>
+        </dependency> 
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("lenovo.pcsd.products.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("新零售权限控制API")
+                .description("lenovo-pcsd-products-media api")
+//                .termsOfServiceUrl("http://blog.didispace.com/")
+//                .contact("")
+                .version("1.0")
+                .build();
+    }
+
+
+}
